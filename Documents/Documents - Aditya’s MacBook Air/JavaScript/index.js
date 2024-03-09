075b7409c74cb882b150a5ed0ff3ff174c3cb30b
Aditya Soni <soniadityakumar651@gmail.com>
2024-03-09 20:40:34 +0530

    index.js

---
+++ Documents/Documents - Aditya’s MacBook Air/JavaScript/index.js
@@ -0,0 +1,11 @@
+// const means => once declared can't be changed.
+// var means => can get update in future.
+
+const accountId = 12341;
+let accountEmail = "aditya@google.com"
+var accountPassword = "1234";
+accountCity = "Gwalior";
+
+accountId = 2;
+
+console.log(accountId);
