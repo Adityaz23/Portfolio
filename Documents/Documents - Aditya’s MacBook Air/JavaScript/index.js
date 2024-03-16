1ef2418d37a244e33d498f1244236296b90714eb
Turbobot <turbobot@vercel.com>
2024-03-16 19:44:47 +0530

    feat(create-turbo): apply official-starter transform

--- Documents/Documents - Aditya’s MacBook Air/JavaScript/index.js
+++ Documents/Documents - Aditya’s MacBook Air/JavaScript/index.js
@@ -1,11 +1,28 @@
 // const means => once declared can't be changed.
 // var means => can get update in future.
 
+
 const accountId = 12341;
-let accountEmail = "aditya@google.com"
-var accountPassword = "1234";
-accountCity = "Gwalior";
+const accountEmail = "aditya@google.com";
+const accountPassword = "1234";
+accountCity = "Gwalior"; // not the correct way.
 
-accountId = 2;
 
-console.log(accountId);
+let accountState; // undefined
+
+// accountId = 2; // the value cant be changed because const se aap kuch bhi change nhi kar sakte hai value ko 
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+
+// After updating => 
+
+console.table([accountEmail,accountId,accountPassword, accountCity,accountState])
+
+// Prefer not to use var because of issue in block scope and functional scope.
+
+
+
+
